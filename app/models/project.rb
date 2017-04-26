cc6777de26d00bb5390573a024d7bc7b36d51cda
sean.mee@hotmail.com <sean.mee@hotmail.com>
2017-04-25 23:14:09 -0400

    removed stupid validations for dates on projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,6 @@
 class Project < ActiveRecord::Base
 
 	validates :title, presence: true, length: { maximum: 50}
-	validates :start_date, presence: true
-	validates :due_date, presence: true
 	validates :project_status, presence: true
 	
 	belongs_to :client
