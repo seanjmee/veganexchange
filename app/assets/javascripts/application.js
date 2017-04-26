bdc0802118f4972a5f0f62c02611a363cdce1e80
sean.mee@hotmail.com <sean.mee@hotmail.com>
2017-04-25 22:45:06 -0400

    added jquery turbolinks to fix search bars

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
 //= require dataTables/extras/dataTables.responsive
